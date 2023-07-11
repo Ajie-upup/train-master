da05fe387157de86c42b9cf3b52253209c56c9d7
heyj13 <1651588782@qq.com>
2023-07-11 13:13:33 +0800

    新增网关模块，配置路由转发

---
+++ train-backend/train-gateway/src/main/java/com/ajie/train/gateway/GateWayApplication.java
@@ -0,0 +1,22 @@
+package com.ajie.train.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ajie
+ * @date 2023/7/11
+ * @description:
+ */
+@SpringBootApplication
+public class GateWayApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(GateWayApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+        logger.info("网关模块启动成功！");
+    }
+}
